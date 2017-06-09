3c61a3cd0d4aa7c5b35db17555b691e37a6d0f81
Polmax <pauloalves2250@gmail.com>
2017-06-09 03:03:56 +0100

    login funcional

---
+++ login.php
@@ -0,0 +1,27 @@
+<?php
+echo("addasd");
+$connection = mysqli_connect("localhost", "root", "polmax", "ambrosio"); // Establishing connection with server..
+
+if (!$connection) {
+    echo "Please try later.";
+} else {
+    echo "encontrei a bd";
+    $db = mysqli_select_db("ambrosio", $connection); // Selecting Database.
+}
+$email=$_POST['email1']; // Fetching Values from URL.
+$password= sha1($_POST['password1']); // Password Encryption, If you like you can also leave sha1.
+// check if e-mail address syntax is valid or not
+$email = filter_var($email, FILTER_SANITIZE_EMAIL); // sanitizing email(Remove unexpected symbol like <,>,?,#,!, etc.)
+if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+    echo "Invalid Email.......";
+} else {
+// Matching user input email and password with stored email and password in database.
+    $result = mysqli_query("SELECT * FROM cliente WHERE Email='$email' AND Password='$password'");
+    $data = mysqli_num_rows($result);
+    if ($data==1) {
+        echo "Successfully Logged in...";
+    } else {
+        echo "Email or Password is wrong...!!!!";
+    }
+}
+mysqli_close ($connection); // Connection Closed.
