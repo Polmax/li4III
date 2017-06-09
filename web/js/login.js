ccb52c39e616008c1e316204fc28e23d15bde744
Polmax <pauloalves2250@gmail.com>
2017-06-09 20:05:19 +0100

    menu principal, registar e descrição da aplicação no login

--- web/js/login.js
+++ web/js/login.js
@@ -1,5 +1,9 @@
 $(document).ready(function () {
-    $("#login").click(function () {
+
+   $("#register").click(function () {
+    windows.location.href = "register.html";
+   });
+    $("#login").click(function() {
         var email = $("#email").val();
         var password = $("#password").val();
         // Checking for blank fields.
